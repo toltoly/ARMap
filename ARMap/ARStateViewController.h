624da695628c9552c638b5a71525a37dcfc7dd99
toltoly <wkim@resolute.com>
2014-02-07 10:21:18 -0500

    facebook

---
+++ ARMap/ARStateViewController.h
@@ -0,0 +1,13 @@
+//
+//  ARStateViewController.h
+//  ARMap
+//
+//  Created by Won Kim on 2/6/14.
+//  Copyright (c) 2014 Won Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARStateViewController : UIViewController
+
+@end
---
+++ ARMap/ARStateViewController.m
@@ -0,0 +1,38 @@
+//
+//  ARStateViewController.m
+//  ARMap
+//
+//  Created by Won Kim on 2/6/14.
+//  Copyright (c) 2014 Won Kim. All rights reserved.
+//
+
+#import "ARStateViewController.h"
+
+@interface ARStateViewController ()
+
+@end
+
+@implementation ARStateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
